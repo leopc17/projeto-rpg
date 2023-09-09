9bbebb22cd627e6957077aca25cca6ca06d8c04f
leopc17 <LeonardoEAF45@gmail.com>
2023-09-09 15:18:55 -0300

    Criação do protótipo do projeto

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,10 +1,64 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Personagem;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		Personagem player = new Personagem("Player", 100, 10);
+		Personagem boss = new Personagem("Boss");
+		
+		int dificuldade;
+		boolean fugir = false;
+		
+		System.out.println("Escolha a dificuldade do jogo:\n1. Fácil\n2. Médio\n3. Difícil\n");
+		dificuldade = sc.nextInt();
+		
+		if (dificuldade == 1) {
+			boss.setVida(50);
+			boss.setAtaque(8);
+		} else if (dificuldade == 2) {
+			boss.setVida(100);
+			boss.setAtaque(12);
+		} else if (dificuldade == 3) {
+			boss.setVida(150);
+			boss.setAtaque(15);
+		}
+		
+		while (player.vivo() && boss.vivo()) {
+			System.out.println("Escolha uma ação:\n1. Ataque básico\n2. Tentar fugir\n");
+			int escolha = sc.nextInt();
+			
+			if (escolha == 1) {
+				player.atacar(boss);
+                if (boss.vivo()) {
+                    boss.atacar(player);
+                }
+			} else if (escolha == 2) {
+				fugir = player.tentarFugir();
+				
+				if (fugir) {
+					System.out.println("Você fugiu.");
+					break;
+				} else {
+					System.out.println("Você tentou fugir e falhou.");
+					boss.atacar(player);
+				}
+			}
+			
+		}
+		
+		if (player.vivo() && fugir == false) {
+			System.out.println("Mensagem de vitória");
+		} else if (fugir == false){
+			System.out.println("Mensagem de derrota");
+		}
+		
+		sc.close();
 	}
 
 }
--- src/entities/Personagem.java
+++ src/entities/Personagem.java
@@ -1,5 +1,73 @@
 package entities;
 
-public class Personagem {
+import java.util.Random;
 
+public class Personagem {
+	private String nome;
+	private int vida;
+	private int ataque;
+	
+	public Personagem(String nome, int vida, int ataque) {
+		this.nome = nome;
+		this.vida = vida;
+		this.ataque = ataque;
+	}
+	
+	public Personagem(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public int getAtaque() {
+		return ataque;
+	}
+	
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+	
+	public boolean vivo() {
+		if (vida > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void atacar(Personagem alvo) {
+		Random random = new Random();
+		int dano = random.nextInt(ataque);
+		
+		System.out.println(nome + " atacou " + alvo.nome + " e causou " + dano + " de dano.");
+		
+		alvo.vida -= dano;
+		
+		if(alvo.vida < 0) {
+			alvo.vida = 0;
+		}
+		
+		System.out.println(alvo.nome + " agora tem " + alvo.vida + " pontos de vida");
+	}
+	
+	public boolean tentarFugir() {
+		Random random = new Random();
+		int n = random.nextInt(9);
+		
+		if (n <= 5) {
+			return false;
+		} else {
+			return true;
+		}
+	}	
 }
